dfe2b4076e61994e81f9364050649ba643e47980
Antea P <antea.primorac0@gmail.com>
2025-11-08 00:17:28 +0100

    - added status enum - added a function that filters by date and optionally a status

--- api_client.py
+++ api_client.py
@@ -1,4 +1,13 @@
+from enum import Enum
+
 import requests
+
+
+class Status(Enum):
+    NEW = 1
+    BUILT = 2
+    PRODUCING = 3
+    FINISHED = 4
 
 
 class ApiClient:
@@ -12,7 +21,20 @@ class ApiClient:
         for item in data.json():
             print(item)
 
+    def filter_build_jobs_by_date(self, date1, date2, status=None):
+        print("Filtering build jobs by date...")
+        if status:
+            data = requests.get(
+                f"http://localhost:3001/api/production_builds?startTime_gte={date1}&startTime_lte={date2}&status={status.name}")
+        else:
+            data = requests.get(
+                f"http://localhost:3001/api/production_builds?startTime_gte={date1}&startTime_lte={date2}")
+        for item in data.json():
+            print(item)
+
 
 if __name__ == '__main__':
     client = ApiClient()
-    client.list_build_jobs()
+    # client.list_build_jobs()
+    client.filter_build_jobs_by_date('2025-10-24T09:00:00Z', '2025-10-24T13:30:00Z')
+    client.filter_build_jobs_by_date('2025-10-24T09:00:00Z', '2025-10-29T13:30:00Z', Status.PRODUCING)
