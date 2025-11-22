6b128199da9fb606a2a9974b96840658e0f05217
Antea P <antea.primorac0@gmail.com>
2025-11-22 19:13:54 +0100

    updated for new CHITUBOX version

--- chitubox.py
+++ chitubox.py
@@ -32,72 +32,84 @@ class Chitubox:
             pyautogui.screenshot(f'screenshot_{datetime.now().strftime("%d-%m-%Y")}.png')
 
     def open_file(self, filename: str):
-        self.click("Open.png", region_box=(0, 0, 300, 400))
-        # pyautogui.hotkey('ctrl', 'o')
+        # self.click("Open.png", region_box=(0, 0, 300, 400))
+        pyautogui.hotkey('ctrl', 'o')
         pyautogui.write(filename, interval=0.1)
         pyautogui.press("enter")
 
     def slice(self):
-        self.click("Slice.png", region_box=(int(RESOLUTION.width * 0.8), int(RESOLUTION.height * 0.7), RESOLUTION.width,
-                                            RESOLUTION.height))
+        # self.click("Slice.png", region_box=(int(RESOLUTION.width * 0.8), int(RESOLUTION.height * 0.7), RESOLUTION.width,
+        #                                     RESOLUTION.height))
+        pyautogui.hotkey('s', 'l')
 
     def save(self):
-        start = time.time()
-        print("Generating slice preview...")
-        pyautogui.screenshot("slice_preview.png", region=(int(RESOLUTION.width * 0.25), int(RESOLUTION.height * 0.75),
-                                                          int(RESOLUTION.width * 0.35), int(RESOLUTION.height * 0.85)))
-        for pos in pyautogui.locateAllOnScreen("GeneratingSlicePreview.png", grayscale=True,
-                                               region=(int(RESOLUTION.width * 0.3), int(RESOLUTION.height * 0.75),
-                                                       int(RESOLUTION.width * 0.3), int(RESOLUTION.height * 0.8))):
-            print(pos)
-        end = time.time()
-        print(f"Time taken to generate slice preview was {end - start} seconds")
-        pyautogui.sleep(2)
-        pyautogui.screenshot("save_.png",
-                             region=(int(RESOLUTION.width * 0.8), int(RESOLUTION.height * 0.85),
-                                     int(RESOLUTION.width * 0.85), int(RESOLUTION.height * 0.95)))
-        self.click("Save.png", region_box=(int(RESOLUTION.width * 0.8), int(RESOLUTION.height * 0.85),
-                                           int(RESOLUTION.width * 0.85), int(RESOLUTION.height * 0.95)))
-        pyautogui.sleep(1)
-        pyautogui.press("enter")
-        pyautogui.dragRel(-100, 0, duration=0.5)
-        pyautogui.sleep(30)
-        try:
-            pyautogui.locateOnScreen("AnycubicSave.png", grayscale=True,
-                                     region=(int(RESOLUTION.width * 0.3), int(RESOLUTION.height * 0.75),
-                                             int(RESOLUTION.width * 0.35), int(RESOLUTION.height * 0.85)))
-        except pyautogui.PyAutoGUIException as e:
-            pyautogui.screenshot("save_success_preview.png",
-                                 region=(int(RESOLUTION.width * 0.3), int(RESOLUTION.height * 0.3),
-                                         int(RESOLUTION.width * 0.4), int(RESOLUTION.height * 0.35)))
-            pyautogui.hotkey('alt', 'f4', logScreenshot=True)
+        pyautogui.screenshot("slice.png", region=(int(RESOLUTION.width * 0.7), int(RESOLUTION.height * 0.8),
+                                                  int(RESOLUTION.width * 0.4), int(RESOLUTION.height)))
+        self.click("Slice_v2.png", region_box=(int(RESOLUTION.width * 0.7), int(RESOLUTION.height * 0.8),
+                                               int(RESOLUTION.width * 0.4), int(RESOLUTION.height)))
+        pyautogui.sleep(5)
+        pyautogui.screenshot("save.png", region=(int(RESOLUTION.width * 0.5), int(RESOLUTION.height * 0.04),
+                                                 int(RESOLUTION.width * 0.55), int(RESOLUTION.height * 0.05)))
 
-    def close_bug_report(self):
-        # TODO: refine
-        try:
-            timeout = time.time() + 5
-            pyautogui.screenshot("bug_report.png", region=(int(RESOLUTION.width * 0.3), int(RESOLUTION.height * 0.1),
-                                                           int(RESOLUTION.width * 0.4), int(RESOLUTION.height * 0.2)))
-            while time.time() <= timeout:
-                for pos in pyautogui.locateAllOnScreen("BugReport.png", grayscale=True,
-                                                       region=(int(RESOLUTION.width * 0.3),
-                                                               int(RESOLUTION.height * 0.1),
-                                                               int(RESOLUTION.width * 0.4),
-                                                               int(RESOLUTION.height * 0.2))):
-                    if pos:
-                        pyautogui.hotkey('alt', 'f4', logScreenshot=True)
-                        break
-        except pyautogui.PyAutoGUIException:
-            pass
+        pyautogui.hotkey('ctrl', 'shift', 's', logScreenshot=True)
+        pyautogui.sleep(1)
+        pyautogui.press('enter')
+        pyautogui.sleep(5)
+        pyautogui.screenshot("save_success_preview.png",
+                             region=(int(RESOLUTION.width * 0.3), int(RESOLUTION.height * 0.3),
+                                     int(RESOLUTION.width * 0.4), int(RESOLUTION.height * 0.35)))
+        pyautogui.hotkey('alt', 'f4', logScreenshot=True)
+        pyautogui.sleep(2)
+        pyautogui.screenshot("model_prepare.png", region=(int(RESOLUTION.width * 0.45), 0,
+                                                          int(RESOLUTION.width * 0.5), int(RESOLUTION.height * 0.05)))
+        self.click("BackToModelPrepare.png", region_box=(int(RESOLUTION.width * 0.45), 0,
+                                                         int(RESOLUTION.width * 0.5), int(RESOLUTION.height * 0.05)))
+        pyautogui.sleep(1)
+        pyautogui.hotkey('alt', 'f4', logScreenshot=True)
+        pyautogui.sleep(1)
+        pyautogui.hotkey('ctrl', 'n', logScreenshot=True)
+
+        pyautogui.screenshot("prompt.png")
+        pyautogui.hotkey('alt', 'f4', logScreenshot=True)
+        pyautogui.hotkey('ctrl', 'n', logScreenshot=True)
+
+        self.click("No.png", region_box=(int(RESOLUTION.width * 0.5), int(RESOLUTION.height * 0.5),
+                                         int(RESOLUTION.width * 0.6), int(RESOLUTION.height * 0.55)))
+
+
+def close_bug_report(self):
+    # TODO: refine
+    try:
+        timeout = time.time() + 5
+        pyautogui.screenshot("bug_report.png", region=(int(RESOLUTION.width * 0.3), int(RESOLUTION.height * 0.1),
+                                                       int(RESOLUTION.width * 0.4), int(RESOLUTION.height * 0.2)))
+        while time.time() <= timeout:
+            for pos in pyautogui.locateAllOnScreen("BugReport.png", grayscale=True,
+                                                   region=(int(RESOLUTION.width * 0.3),
+                                                           int(RESOLUTION.height * 0.1),
+                                                           int(RESOLUTION.width * 0.4),
+                                                           int(RESOLUTION.height * 0.2))):
+                if pos:
+                    pyautogui.hotkey('alt', 'f4', logScreenshot=True)
+                    break
+    except pyautogui.PyAutoGUIException:
+        pass
 
 
 if __name__ == '__main__':
     chitubox = Chitubox()
     pyautogui.sleep(10)
-    # TODO: check if there's 'restore project files prompt', or if there's report bug prompt
-
     # chitubox.close_bug_report()
-    chitubox.open_file("calicat.stl")
-    pyautogui.sleep(2)
-    chitubox.slice()
-    chitubox.save()
+    # TODO: check if there's 'restore project files prompt', or if there's report bug prompt
+    for file in os.scandir():
+        if file.name.endswith(".stl"):
+            chitubox.open_file(file.name)
+            pyautogui.sleep(2)
+            chitubox.slice()
+            chitubox.save()
+            # for file in os.scandir():
+            #     if file.name.endswith(".stl"):
+            #         chitubox.open_file(file.name)
+            #         pyautogui.sleep(2)
+            #         chitubox.slice()
+            #         chitubox.save()
