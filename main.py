5f3363d9b431d35602e9d78e6c9f52c105d4c662
Antea P <antea.primorac0@gmail.com>
2025-12-12 23:00:41 +0100

    refactored main

--- main.py
+++ main.py
@@ -1,74 +1,45 @@
-import time
-
-import arrow
+from typing import Any
 
 import api_client
-from custom_types import Status
+import service
 
 client = api_client.ApiClient()
 
 
-def get_filtered_builds_3_days_span() -> list[dict]:
-    def is_build_eligible(build_) -> bool:
-        if build_.get('status') == Status.NEW:
-            return False
-
-        if 'batches' not in build_:
-            return False
-
-        batch_unit_count = build_.get('batches').get('batchUnitCount', 0)
-        if batch_unit_count == 0:
-            return False
-
-        if 'slicePath' not in build_:
-            return False
-
-        return True
-
+def get_filtered_builds_3_days_span() -> list[Any] | None:
     # https://arrow.readthedocs.io/en/latest/guide.html
-    now = arrow.now()
-    three_days_ago = now.shift(days=-3)
-    in_three_days = now.shift(days=3)
+    recent_builds = service.get_build_jobs_between(days_in_past=-3, days_in_future=3)
 
-    recent_builds = client.get_build_jobs(three_days_ago.format('YYYY-MM-DD[T]HH:mm:ss[Z]'),
-                                          in_three_days.format('YYYY-MM-DD[T]HH:mm:ss[Z]'))
-
-    filtered_builds = []
-
-    for build in recent_builds:
-        if is_build_eligible(build):
-            filtered_builds.append(build)
-
-    filtered_builds.sort(key=lambda x: x.get('startTime'))
-
-    return filtered_builds
-
-
-# Close to main so it's colocated to functions that use it
-already_seen_build_ids = set()
+    return service.filter_and_sort_builds(builds=recent_builds, sort_key='startTime')
 
 
 def main():
-    while True:
-        eligible_builds = get_filtered_builds_3_days_span()
-        eligible_builds = [build for build in eligible_builds if build['id'] not in already_seen_build_ids]
-        if not eligible_builds:
-            print("No builds in queue, will sleep for 5 minutes. (－ω－) zzZ")
-            time.sleep(5 * 60)
-            continue
-        for build in eligible_builds:
-            print(build)
-            print("--------------------------------")
-        highest_priority_build = eligible_builds[0]  # Because it's already sorted
+    already_seen_build_ids = set()
 
-        try:
-            already_seen_build_ids.add(highest_priority_build['id'])
-            # TODO
-            # process_build(highest_priority_build['id'])
-        except Exception as error:
-            print(
-                f"Build for {highest_priority_build['id']} failed, will skip to next eligible build. Error message: {error}")
-            continue
+    while True:
+        filtered_builds = get_filtered_builds_3_days_span()
+        if filtered_builds:
+            eligible_builds = [build for build in filtered_builds if build['id'] not in already_seen_build_ids]
+        else:
+            print("No builds in queue, terminating the script...")
+            return
+
+        if eligible_builds:
+            for build in eligible_builds:
+                print(build)
+                print("--------------------------------")
+            highest_priority_build = eligible_builds[0]  # Because it's already sorted
+
+            try:
+                if highest_priority_build['id'] not in already_seen_build_ids:
+                    already_seen_build_ids.add(highest_priority_build['id'])
+                    # TODO
+                    # process_build(highest_priority_build['id'])
+                break
+            except Exception as error:
+                print(
+                    f"Build for {highest_priority_build['id']} failed, will skip to next eligible build. Error message: {error}")
+                continue
 
 
 if __name__ == "__main__":
